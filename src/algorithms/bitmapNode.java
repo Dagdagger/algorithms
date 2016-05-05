31a818c2eb378e9de4a8ad4cb54a3ee13de66d6e
Dagdagger <omar_brt@yahoo.com>
2016-05-04 21:42:35 -0700

    Updated to New Code
    Code emailed online from Root

--- src/algorithms/bitmapNode.java
+++ src/algorithms/bitmapNode.java
@@ -1,16 +1,19 @@
 package algorithms;
 
-public class bitmapNode {
-	int bitmap;
-	bitmapNode link;
-	
-	public bitmapNode() {
-		bitmap = 0;
-		link = null;
-		
-	}
-	public bitmapNode(int map) {
-		bitmap = map;
-		link = null;
-	}
+public class bitmapNode
+{
+  int         bitmap ;
+  bitmapNode  link   ;
+  
+  public bitmapNode( )
+  {
+     bitmap = 0 ;
+     link   = null ;
+  }
+
+  public bitmapNode( int map )
+  {
+     bitmap = map ;
+     link   = null ;
+  }
 }
--- src/algorithms/point3D.java
+++ src/algorithms/point3D.java
@@ -1,19 +1,22 @@
 package algorithms;
 
 
-public class point3D {
-		int x; 
-		int y;
-		int z;
-		
-		public point3D() {
-			
-		}
-		public point3D (int ix, int iy, int iz){
-			x = ix;
-			y = iy;
-			z = iz;
-			
-		}
-		
-	}
+public class point3D
+{
+  int x ; 
+  int y ; 
+  int z ;
+  
+  public point3D( ) 
+  {
+  }
+
+  public point3D( int ix , int iy , int iz ) 
+  {
+    x = ix ;
+    y = iy ; 
+    z = iz ;
+  }
+
+}
+
